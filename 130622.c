e5ba73da934e561da3794f1661babffacab86ebb
Tranthuhang1 <106530910+Tranthuhang1@users.noreply.github.com>
2022-06-14 22:20:40 +0700

    Add files via upload

---
+++ 130622.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void main() {
+
+int count;
+printf("\tThis is a\n");
+	for(count=1; count<=100;count++){
+	printf("\tThis is  count\n");
+		printf("%d",count);
+		printf("n\t\t nice");
+	}
+	printf("n\t\t world");
+	
+}
